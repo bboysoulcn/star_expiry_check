6a4d2dbca1ee0cc1c9d38adf8d74afeab520c145
bboysoul <bboysoulcn@gmail.com>
2025-12-31 09:19:18 +0800

    1. first commit

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ from rich.console import Console
 from rich.syntax import Syntax
 import json
 import os
-import functools
 
 token = os.environ.get('TOKEN')
 client = httpx.AsyncClient()
@@ -18,8 +17,6 @@ semaphore = asyncio.Semaphore(100)
 console = Console()
 
 
-# 缓存获取关注仓库的结果
-@functools.lru_cache(maxsize=1)
 # 获取关注的仓库
 async def get_followed_repos():
     headers = {
@@ -39,9 +36,6 @@ async def get_followed_repos():
             else:
                 for repo in response:
                     all_repo.append(repo["full_name"])
-        else:
-            logger.error(f"获取仓库失败，状态码: {response.status_code}")
-            break
     logger.info("获取到的 repo 为" + str(len(all_repo)))
     return all_repo
 
@@ -78,10 +72,10 @@ async def get_data(repo):
 async def main():
     all_repo = await get_followed_repos()
     tasks = [get_data(repo) for repo in all_repo]
-    results = await asyncio.gather(*tasks, return_exceptions=True)
+    results = await asyncio.gather(*tasks)
 
-    # 过滤掉 None 值和异常
-    filtered_results = [result for result in results if result and not isinstance(result, Exception)]
+    # 过滤掉 None 值并创建最终的 JSON 对象
+    filtered_results = [result for result in results if result]
     json_output = json.dumps(filtered_results, ensure_ascii=False, indent=2)
 
     # 使用 rich 来美化输出
